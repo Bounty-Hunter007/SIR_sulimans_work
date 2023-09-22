67453c94a90ffa4f4d36196711fa9c72c91a5eb5
SYED MUHAMMAD ASH-HADU FARIDI <117972508+Bounty-Hunter007@users.noreply.github.com>
2023-09-22 17:15:59 +0500

    Delete src directory

--- src/components/CustomBtn.js
+++
@@ -1,9 +0,0 @@
-function CustomBtn () {
-  return (
-    <div>
-      <button style={{ backgroundColor: 'yellow', height: 50, width: 100 }}>Contact Me</button>
-    </div>
-  )
-}
-
-export default CustomBtn
