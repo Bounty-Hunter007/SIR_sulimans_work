4bea7c75ed602796c5f2bc0b87a189da11c841d6
SYED MUHAMMAD ASH-HADU FARIDI <117972508+Bounty-Hunter007@users.noreply.github.com>
2023-09-22 17:16:41 +0500

    Add files via upload

---
+++ components/CustomBtn.js
@@ -0,0 +1,9 @@
+function CustomBtn () {
+  return (
+    <div>
+      <button style={{ backgroundColor: 'yellow', height: 50, width: 100 }}>Contact Me</button>
+    </div>
+  )
+}
+
+export default CustomBtn
