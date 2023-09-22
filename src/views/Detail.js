0c4220e2d434a3e35b9b1cc74b31a03811594b48
SYED MUHAMMAD ASH-HADU FARIDI <117972508+Bounty-Hunter007@users.noreply.github.com>
2023-09-22 17:14:46 +0500

    Add files via upload

---
+++ src/views/Detail.js
@@ -0,0 +1,11 @@
+function Detail() {
+  return (
+    <div style={{ backgroundColor: 'seagreen', height: '100vh' }}>
+      <h1>Detail</h1>
+
+      <p>This is Detail PAGE</p>
+    </div>
+  );
+}
+
+export default Detail;
---
+++ src/views/PostAD.js
@@ -0,0 +1,12 @@
+function PostAD() {
+    
+    return(
+        <div style={{ backgroundColor: 'navy', height: '100vh' }}>
+            <h1>PostAD</h1>
+
+            <p>This is PostAD PAGE</p>
+        </div>
+    )
+}
+
+export default PostAD
