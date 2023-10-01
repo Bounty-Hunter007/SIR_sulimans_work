2e52d6459dcaec1895611e9c53c3aaaa705e6e57
SYED MUHAMMAD ASH-HADU FARIDI <117972508+Bounty-Hunter007@users.noreply.github.com>
2023-10-01 11:14:33 +0500

    Delete components directory

--- components/Card.js
+++
@@ -1,9 +0,0 @@
-function Card() {
-  return (
-    <div style={{ height: 300, width: 300, border: '1px solid black', background: 'white', boxShadow: 'black 7px 13px 17px -2px', margin: 5 }}>
-      Card
-    </div>
-  )
-}
-
-export default Card
